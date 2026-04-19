6a7b945853d552ea0ab1ba35bcc8f92697069200
AlexAkama <gremcox@bk.ru>
2026-04-19 12:24:29 +0500

    Добавил стили кнопкам управления

--- src/main/java/org/example/tm66/controller/UIController.java
+++ src/main/java/org/example/tm66/controller/UIController.java
@@ -6,6 +6,7 @@ import org.example.tm66.util.TimeUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,12 +20,14 @@ public class UIController {
     }
 
     @GetMapping("/groups")
-    public String showGroups(Model model) {
+    public String showGroups(Model model,
+                             @RequestParam(name = "admin", required = false, defaultValue = "false") boolean admin
+    ) {
         model.addAttribute("groups", orderService.getGroups());
         model.addAttribute("nowEnd", orderService.getNowEndOrderId());
         model.addAttribute("returned", orderService.getReturnedOrderId());
         model.addAttribute("now", TimeUtils.now());
-        model.addAttribute("admin", false);
+        model.addAttribute("admin", admin);
         return "groups";
     }
 
